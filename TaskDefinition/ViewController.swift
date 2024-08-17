ce4d739190e67a49c645315926b63a0cd1de0e72
azimxxm <azimxxm@gmail.com>
2024-08-18 00:11:52 +0500

    Initial Commit

---
+++ TaskDefinition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskDefinition
+//
+//  Created by Azimjon Abdurasulov on 18/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
